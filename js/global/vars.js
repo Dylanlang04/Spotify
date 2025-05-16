0ef30bab0b48ff493b0a9f9f8db90d3ca1dc09bd
dylanlang04 <dylanlangtrade@gmail.com>
2025-05-16 19:58:32 -0400

    Home Page

---
+++ js/global/vars.js
@@ -0,0 +1,8 @@
+const volume = document.getElementById("volumeSlider")
+const home = document.getElementById("home_btn")
+
+
+const vol_icon = document.getElementsByClassName("volume")
+
+
+const page = document.getElementById("fullscreen1")
--- js/load.js
+++ js/load.js
@@ -2,4 +2,5 @@
 
 document.addEventListener("DOMContentLoaded", () => {
     loadListeners()
+    homePage()
 })
