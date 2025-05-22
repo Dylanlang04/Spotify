e48d72d1875c7509994346bd289a6b11e1769262
dylanlang04 <dylanlangtrade@gmail.com>
2025-05-21 22:52:24 -0400

    browse, layout. made it expandable. browse is ready for backend work

---
+++ js/buttons/browse/browse.js
@@ -0,0 +1,18 @@
+function browsePage() {
+    page.innerHTML = ""
+    page.innerHTML += `<div id="browse_wrapper">
+    <div class="browseall">Browse all</div>
+    <div id="categories">
+    <button class="category"><div class="cat_title">TITLE</div></button>
+    <button class="category"><div class="cat_title">TITLE</div></button>
+    <button class="category"><div class="cat_title">TITLE</div></button>
+    <button class="category"><div class="cat_title">TITLE</div></button>
+    <button class="category"><div class="cat_title">TITLE</div></button>
+    </div>
+    
+    
+    </div>
+    
+    `
+
+}
