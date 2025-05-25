237930d2e636f639b6178510a2980550cdddccdc
dylanlang04 <dylanlangtrade@gmail.com>
2025-05-24 22:02:37 -0400

    queue button + view. Need to add handling for when a view is already active

---
+++ js/resize.js
@@ -0,0 +1,11 @@
+function updateScreenPosition() {
+    if (view === 1) {
+        const playviewWidth = playingView.getBoundingClientRect().width
+        fullscreen.style.right = `${playviewWidth + 20}px`
+    } else if (view1 === 1) {
+        const queueWidth = queueView.getBoundingClientRect().width
+        fullscreen.style.right = `${queueWidth + 20}px`
+    }
+
+    
+}
