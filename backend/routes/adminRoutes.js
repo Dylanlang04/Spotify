797984a00bc3626d17fe86f02f14c5f74d6275f7
dylanlang04 <dylanlangtrade@gmail.com>
2025-06-25 22:08:54 -0400

    top 10 worst business decisions. coming in at number 1, Spotify gatekeeping their developer api for literally no reason other than to be asswipes. spent 5 hours trying to get data from an endpoint that is being gatekept and they told nobody. requirement: create an app using spotify's public api of data widely available and reletively useless data to build an app that pulls in 250k users.

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -6,7 +6,10 @@ const jwt = require('jsonwebtoken')
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
 const multer = require('multer')
 const upload = multer()
-
+const mm = require('music-metadata')
+const fs = require('fs')
+const clientId = process.env.SPOTIFYCLIENT
+const clientSecret = process.env.SPOTIFYSECRET
 const jwt_secret = process.env.JWTSECRET
 
 const s3Client = new S3Client({
@@ -16,6 +19,76 @@ const s3Client = new S3Client({
     accessKeyId: process.env.GODACCESS,
     secretAccessKey: process.env.GODSECRET,
   },
+})
+
+
+
+async function getSpotifyAccessToken() {
+  const creds = Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
+  const res = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization: "Basic " + creds,
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "grant_type=client_credentials"
+  })
+
+  const data = await res.json()
+  if (!data.access_token) {
+    throw new Error(`Failed to get access token: ${JSON.stringify(data)}`);
+  }
+  return data.access_token
+}
+async function searchSpotifyTrack(title, artist, accessToken) {
+  const query = `${encodeURIComponent(title)} artist:${encodeURIComponent(artist)}`
+  const url = `https://api.spotify.com/v1/search?q=${query}&type=track&limit=1`
+
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  })
+  const data = await res.json()
+  const track = data.tracks.items[0]
+
+  if (!track) return null
+
+  return {
+    spotify_track_id: track.id,
+    cover_image_url: track.album.images[0]?.url || null
+  }
+}
+
+router.get('/cover/:title/:artist', async (req, res) => {
+    const token = await getSpotifyAccessToken()
+    const {title, artist} = req.params
+    const {spotify_track_id, cover_image_url} = await searchSpotifyTrack(title, artist, token)
+    return res.json({spotify_track_id, cover_image_url})
+})
+const trackId = '7ouMYWpwJ422jRcDASZB7P'
+
+
+router.get('/song/track/:trackid', async (req, res) => {
+  
+  const trackid = req.params.trackid
+  const url = `https://api.spotify.com/v1/tracks/${trackid}`
+  const token = await getSpotifyAccessToken()
+  console.log(token)
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ` + token,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
+  })
+  if (!response.ok) {
+    const errorData = await response.json()
+    console.error('Spotify API error:', errorData)
+    return res.status(response.status).json(errorData)
+  }
+  const data = await response.json()
+  return res.json({data})
 })
 
 
@@ -47,7 +120,6 @@ router.get('/:token', async (req,res) => {
       client.connect()
       const result = await client.query("SELECT * FROM users WHERE id = $1 AND perms = $2", [userId, 'admin'])
       client.end()
-      console.log(result)
       res.json({user: result.rows[0]})
     }
   } )
@@ -56,7 +128,7 @@ router.get('/:token', async (req,res) => {
 })
 router.post('/upload', upload.single('file'), async (req, res) => {
   const file = req.file
-
+  const metadata = JSON.parse(req.body.metadata)
   if (!file) return res.status(400).send('No file uploaded.')
 
   const command = new PutObjectCommand({
@@ -64,7 +136,8 @@ router.post('/upload', upload.single('file'), async (req, res) => {
     Key: file.originalname,
     Body: file.buffer,
     ContentType: file.mimetype,
-  });
+  })
+  
 
   try {
     await s3Client.send(command)
@@ -74,6 +147,19 @@ router.post('/upload', upload.single('file'), async (req, res) => {
     res.status(500).json({message: "upload failed"})
   }
 })
+
+router.get("/songs", async (req, res) => {
+  const client = new Client({
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+  })
+  client.connect()
+  const result = await client.query("SELECT * FROM tracks")
+  client.end()
+  res.json({songs: result.rows})
+})
+
 
 
 
--- js/admin/dev.js
+++ js/admin/dev.js
@@ -2,6 +2,23 @@ const submit = document.getElementById("submit")
 const form = document.getElementById("upload")
 const fileinp = document.getElementById("file")
 
+function parseCSV(input) {
+    
+    if (!input) return []
+    return input.split(',').map(s => s.trim()).filter(Boolean)
+}
+function getVal(name) {
+    const val = form.elements[name].value.trim()
+    return val === '' ? null : val
+}
+function parseNumber(input) {
+  if (!input) return null;
+  const n = Number(input);
+  return isNaN(n) ? null : n;
+}
+
+
+
 
 submit.addEventListener("click", async (e) => {
     e.preventDefault()
@@ -9,20 +26,110 @@ submit.addEventListener("click", async (e) => {
     const files = fileinp.files
 
      if (!files.length) {
-        alert("No file selected.")
-        return
+        //alert("No file selected.")
+        //return
+    }
+    const jsonObject = {}
+    if (getVal('spotify_track_id')) {
+        const responsetrack = await fetch(`http://localhost:3000/api/admin/song/track/${getVal('spotify_track_id')}`)
+        const resulttrack = await responsetrack.json()
+        jsonObject['album'] = resulttrack.data.album.name
+        jsonObject['main_artist'] = resulttrack.data.artists[0]
+        if (resulttrack.data.artists.length > 1) {
+            let artists = []
+            for (let i = 0; i < resulttrack.data.artists.length - 1; i++) {
+                artists[i] = resulttrack.data.artists[1 + i]    
+            }
+            jsonObject['featured_artists'] = artists
+        }
+        jsonObject['title'] = resulttrack.data.name
+        jsonObject['spotify_track_id'] = resulttrack.data.id
+        jsonObject['popularity'] = resulttrack.data.popularity
+        jsonObject['isrc'] = resulttrack.data.external_ids.isrc
+        jsonObject['explicit'] = resulttrack.data.explicit
+        jsonObject['duration_ms'] = resulttrack.data.duration_ms
+        jsonObject['disc_num'] = resulttrack.data.disc_number
+        jsonObject['release_date'] = resulttrack.data.album.release_date
+        
+        if (resulttrack.data.album.images.length > 0) {
+            let images = []
+            for (let i = 0; i < resulttrack.data.album.images.length; i++) {
+                images[i] = resulttrack.data.album.images[i]
+            }
+            jsonObject['images'] = images
+        } 
+        
+
+        
+
+        console.log(resulttrack)
+        console.log(jsonObject)
+        
+    } else {
+        jsonObject.title = getVal('title')
+        jsonObject.main_artist = getVal('main_artist')
+        jsonObject.featured_artists = parseCSV(getVal('featured_artists'))
+        let fields =['album', 'genre', 'producer', 'writer', 'performer', 'key', 'isrc_code', 'artist_id', 'spotify_track_id', 'lyrics_url', 'cover_image_url']
+        fields.forEach(field => {
+            const val = getVal(field)
+            jsonObject[field] = val
+        })
+        let numFields = ['duration_ms', 'popularity', 'total_plays', 'skip_rate', 'like_count', 'bpm']
+        numFields.forEach(field => {
+            const val = parseNumber(getVal(field))
+            jsonObject[field] = val
+        })
+
+        const explicitVal = getVal('explicit')
+        if (explicitVal === 'true') {
+            jsonObject.explicit = true
+        } else if (explicitVal === 'false') {
+            jsonObject.explicit = false
+        }
+
+        const formData = new FormData()
+        formData.append("file", files[0])
+
+        formData.forEach((value, key) => {
+
+            if (value === 'true') {
+              jsonObject[key] = true
+            } else if (value === 'false') {
+              jsonObject[key] = false
+            } else if (!isNaN(value) && value.trim() !== '') {
+              jsonObject[key] = Number(value)
+            } else {
+              jsonObject[key] = value.trim()
+            }
+        })
+        const response = await fetch(`http://localhost:3000/api/admin/cover/${jsonObject.title}/${jsonObject.main_artist}`)
+        const result1 = await response.json()
+        jsonObject['spotify_track_id'] = result1.spotify_track_id
+        jsonObject['cover_image_url'] = result1.cover_image_url
+        console.log(result1.spotify_track_id)
+    
+    
+        formData.append('metadata', JSON.stringify(jsonObject))
+        const res = await fetch('http://localhost:3000/api/admin/upload', {
+            method: "POST",
+            body: formData,
+        })
+        const result = await res.json()
+        console.log(result)
     }
 
-    const formData = new FormData()
-    formData.append("file", files[0])
-    const res = await fetch('http://localhost:3000/api/admin/upload', {
-        method: "POST",
-        body: formData,
-    })
-    const result = await res.json()
-    console.log(result)
+    
+    
 })
 
 document.getElementById("back").addEventListener("click", ()=>{
     window.location.href = '/index.html'
+})
+
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const res = await fetch("http://localhost:3000/api/admin/songs")
+    const result = await res.json()
+    
 })
